ab7c0c46bdc777c544979b572bc64d82093e62e3
Saga Kortesaari <sagak@student.chalmers.se>
2020-11-01 00:39:02 +0100

    fetching songs medium term

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,13 @@
 $("#login-button").submit(function () {
     var client_id = "883553e0c62a4876a9a81a39f65d34ef";
     var redirect_uri = "http://api.dcronqvist.se:12432";
+    var scope = "user-top-read";
 
     var url = "https://accounts.spotify.com/authorize";
     url += "?client_id=" + client_id;
     url += "&response_type=token";
     url += "&redirect_uri=" + redirect_uri;
+    url += "&scope=" + scope;
 
     window.location = url;
     return false;
@@ -27,13 +29,23 @@ $("#login-button").submit(function () {
 
     if (access_token) {
         $.ajax({
-            url: "https://api.spotify.com/v1/me",
+            url: "https://api.spotify.com/v1/me/top/tracks",
             headers: {
                 Authorization: "Bearer " + access_token,
             },
+            data: {
+                time_range: "medium_term",
+            },
             success: function (response) {
-                console.log(response);
-                $("#answer").html("<img src=" + response.images[0].url + ">");
+                for (let index = 0; index < 20; index++) {
+                    let tracks = document.getElementById("toptracks");
+                    let newtrack = document.createElement("div");
+                    newtrack.innerHTML =
+                        response.items[index].name +
+                        " by " +
+                        response.items[index].artists[0].name;
+                    tracks.appendChild(newtrack);
+                }
             },
         });
     }
