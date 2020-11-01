476a7b51a80987df7c202a90b53d028b5d3c9ec0
Saga Kortesaari <sagak@student.chalmers.se>
2020-11-01 13:24:29 +0100

    redid callajax function + now checking state parameter stored in browser

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,3 @@
-$("#login-button").submit(function () {
-    var client_id = "883553e0c62a4876a9a81a39f65d34ef";
-    var redirect_uri = "http://api.dcronqvist.se:12432";
-    var scope = "user-top-read";
-
-    var url = "https://accounts.spotify.com/authorize";
-    url += "?client_id=" + client_id;
-    url += "&response_type=token";
-    url += "&redirect_uri=" + redirect_uri;
-    url += "&scope=" + scope;
-
-    window.location = url;
-    return false;
-});
-
 function topsongs(response, div) {
     for (let index = 0; index < 20; index++) {
         let tracks = document.getElementById(div);
@@ -25,9 +10,19 @@ function topsongs(response, div) {
     }
 }
 
-function callajax(term, action, type, token, div) {
+/*
+    callajax function
+    term: time-range for the intended request
+    action: function to call at success
+    path: path parameter
+    token: access_token generated from API
+    div: what div to append result to
+
+*/
+
+function callajax(term, action, path, token, div) {
     $.ajax({
-        url: "https://api.spotify.com/v1/me/top/" + type,
+        url: "https://api.spotify.com/v1/me/top/" + path,
         headers: {
             Authorization: "Bearer " + token,
         },
@@ -38,47 +33,81 @@ function callajax(term, action, type, token, div) {
             action(response, div);
         },
         error: function () {
-            $("#results").attr("class", "noshow");
-            $("#login").removeAttr("class", "noshow");
+            $("#results").hide();
+            $("#login").show();
         },
     });
 }
 
+function encodeURL() {
+    var split = window.location.href.split("#");
+    if (split[1]) {
+        split = split[1].split("&");
+        var fragments = {
+            access_token: split[0].split("=")[1],
+            token_type: split[1].split("=")[1],
+            expires_in: split[2].split("=")[1],
+            state: split[3].split("=")[1],
+        };
+        return fragments;
+    }
+}
+
 (function () {
-    var access_token;
+    var stateKey = "spotify_auth_state";
     var results = $("#results");
+    var login = $("#login");
 
-    function encodeURL() {
-        var split = window.location.href.split("#");
-        if (split[1]) {
-            split = split[1].split("&");
-            split = split[0].split("=");
-            access_token = split[1];
+    var fragments = encodeURL();
+
+    if (fragments != null) {
+        var currentState = fragments.state;
+        var storedState = localStorage.getItem(stateKey);
+
+        if (fragments.access_token && currentState == storedState) {
+            results.show();
+            login.hide();
+            localStorage.removeItem(stateKey);
+
+            callajax(
+                "short_term",
+                topsongs,
+                "tracks",
+                fragments.access_token,
+                "toptracks-shortterm"
+            );
+
+            callajax(
+                "medium_term",
+                topsongs,
+                "tracks",
+                fragments.access_token,
+                "toptracks-mediumterm"
+            );
+        } else {
+            login.show();
+            results.hide();
         }
-    }
-
-    encodeURL();
-
-    if (access_token) {
-        results.removeAttr("class", "noshow");
-        $("#login").attr("class", "noshow");
-
-        callajax(
-            "short_term",
-            topsongs,
-            "tracks",
-            access_token,
-            "toptracks-shortterm"
-        );
-
-        callajax(
-            "medium_term",
-            topsongs,
-            "tracks",
-            access_token,
-            "toptracks-mediumterm"
-        );
     } else {
-        results.attr("class", "noshow");
+        login.show();
+        results.hide();
     }
+
+    $("#login-button").submit(function () {
+        var client_id = "883553e0c62a4876a9a81a39f65d34ef";
+        var redirect_uri = "http://api.dcronqvist.se:12432";
+        var scope = "user-top-read";
+        var state = Math.floor(Math.random() * 10000);
+        localStorage.setItem(stateKey, state);
+
+        var url = "https://accounts.spotify.com/authorize";
+        url += "?client_id=" + client_id;
+        url += "&response_type=token";
+        url += "&redirect_uri=" + redirect_uri;
+        url += "&state=" + state;
+        url += "&scope=" + scope;
+
+        window.location = url;
+        return false;
+    });
 })();
